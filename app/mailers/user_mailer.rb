935c969e9870c5a649c08275d2722f766b299799
Andrew Tran <s3455476@student.rmit.edu.au>
2016-04-04 22:48:26 +1000

    add confimrable to users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
 
-  def welcome_email(user)
+  def registration_mail(user,admin)
+    @admin = admin
     @user = user
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Presenter Registration Requires Approval.')
