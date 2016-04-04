5429fd476c8ea9cfd24f0c08db2e42ffbdb97c6d
Frank Cho <s3449016@student.rmit.edu.au>
2016-04-05 05:29:39 +1000

    add user controller

---
+++ app/controllers/users/users_controller.rb
@@ -0,0 +1,16 @@
+class Users::UsersController < ApplicationController
+
+def new
+
+end
+
+
+def create
+
+    unless current_user.user_type == 2 && current_user.status == 1
+        redirect_to :back, :alert => "Admin Access denied."
+    end
+
+end
+
+end
