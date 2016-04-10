53d5a82281d94833602bc7667f59e058453684fe
Frank Cho <s3449016@student.rmit.edu.au>
2016-04-11 04:38:48 +1000

    take out print messages

--- app/services/vit_validation.rb
+++ app/services/vit_validation.rb
@@ -6,13 +6,11 @@ class VitValidation
 	def check_vit(presenterFirstName, presenterLastName, presenterVIT)
 		page_url = "http://www.vit.vic.edu.au/search-the-register/_nocache?first_name=" + presenterFirstName + "&last_name=" + presenterLastName + "&reg_number=" + presenterVIT
 	  page = Nokogiri::HTML(open(page_url))   
-	  
+
 		# if this div does exist which indicates no result
 		if !page.at_css('div#content_container_1727 p').nil?
-			print "false1"
 			return false   # return false if this div and contents of the paragraph exist meaning USER does not exist
 		else
-			print "true"
 			return true	   # return true if this div and contents of this paragraph do not exist meaning user DOES exist
 		end
 	end
