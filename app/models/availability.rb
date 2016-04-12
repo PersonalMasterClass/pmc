feb0b05a4a1374eb4f0accf35930c3a5dd83c94e
Jack Kelly <s3449789@student.rmit.edu.au>
2016-04-12 21:26:16 +1000

    abstracted time methods to TimeTools class

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,38 +1,17 @@
 class Availability < ActiveRecord::Base
 
 	def get_days
-		return this.days.to_s(2).rjust(7, '0')
+		# parse int (base 10) to char array of base 2 value
+		x = this.days.to_s(2).rjust(7, '0').split(//)
+		#convert to boolean array
+		for i in 0..x.length
+			x[i] = (x[i] != 0)
+		end
+		return x
 	end
 
 	def set_days(days)
 		this.days = Integer(days, 2)
 		this.save
-	end
-
-	#TODO error checking for parsing
-	def time_from_string(str)
-		tm = str.split(':')
-		h = tm[0].to_i
-		m = tm[1].to_i
-
-		return h * 60 + m
-	end
-
-	def get_start_time_string
-		return get_hours(this.start_time).rjust(2,'0') + ":" + 
-			get_minutes(this.start_time).rjust(2,'0')
-	end
-
-	def get_end_time_string
-		return get_hours(this.end_time).rjust(2,'0') + ":" + 
-			get_minutes(this.end_time).rjust(2,'0')
-	end
-	# time defined by minutes past midnight. 
-	def get_hours(time)
-		return (time/60).floor
-	end
-
-	def get_minutes(time)
-		return (time % 60).floor
 	end
 end
