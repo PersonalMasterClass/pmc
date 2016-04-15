43d57b70a5f3fb0df90236e762382ddde2648349
Frank Cho <s3449016@student.rmit.edu.au>
2016-04-16 02:25:13 +1000

    add booking model and add associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ActiveRecord::Base
+  has_many :booked_customers
+  has_many :bookings, through: :booked_customers
+
 	belongs_to :school_info
 	validates :first_name, :last_name, :phone_number,
 						:vit_number, :department, :contact_title, presence: true
