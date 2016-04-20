0d2b364859bf436b8e3bbf8ea5f49e7415af3d42
Andrew Tran <s3455476@student.rmit.edu.au>
2016-04-20 12:27:20 +1000

    refactor nav

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,11 +1,2 @@
 module BookingsHelper
-	def display_nav
-		if current_user.user_type == "presenter"
-			render "presenters/partials/presenter_nav"
-		elsif current_user.user_type == "customer"
-			render "customers/partials/customer_nav"
-		else
-			render "users/partials/admin_nav"
-		end
-	end		
 end
