a18dad557e99f5ebbfb4fc83eecb93727ac90e31
Jack Kelly <s3449789@student.rmit.edu.au>
2016-04-22 20:57:42 +1000

    form passing values

--- app/controllers/availabilitys_controller.rb
+++ app/controllers/availabilitys_controller.rb
@@ -14,17 +14,28 @@ class AvailabilitysController < ApplicationController
 	# submit form
 	def create
 		# redirect_to action: 'index'
-		render json: params
+
+		availability = Availability.new
+		availability.start_time = set_time(params['start_time'])
+		availability.end_time = set_time(params['end_time'])
+
+		render json: params['aval_days']
 	end
 
 
-	# private
+	private
 	# def availability_params
  #      params.require(:availability).permit(:day_val, :start_time, :end_time)
  #    end
-
+ 	def set_time(time_str)
+ 		x = time_str.split(':')
+ 		return x[0].to_i * 60 + x[1].to_i
+ 	end
   def set_day(daysArray)
   	dat = 0	
+
+	end
+	def day_arr
 
 	end
 end
