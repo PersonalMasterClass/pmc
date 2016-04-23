da27bc3f5a8af37cce119cba358e9b0b43a6787f
Tyler Goodwin <tyler.goodwin23@gmail.com>
2016-04-23 15:33:13 +1000

    fix dragonfly config

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -9,19 +9,19 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  # if Rails.env.development? || Rails.env.test?
-    # datastore :file,
-    #         root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    #         server_root: Rails.root.join('public')
+  if Rails.env.development? || Rails.env.test?
+    datastore :file,
+            root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+            server_root: Rails.root.join('public')
 
-  # else
+  else
     datastore :s3,
       region: 'ap-southeast-2',
       bucket_name: 'personalmasterclass-dev',
-      access_key_id: 'AKIAJ4YR5WINZR5IMHCA',
-      secret_access_key: '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',
+      access_key_id: '',
+      secret_access_key: '',
       url_scheme: 'https'
-  # end
+  end
 
   fetch_file_whitelist [/public/]
 end
