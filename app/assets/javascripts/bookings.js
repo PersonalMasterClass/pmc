0783c19ea8394e48788d1dae66edfcde56799b5a
Jack Kelly <s3449789@student.rmit.edu.au>
2016-04-25 19:54:57 +1000

    Revert "crudely added subject auto-complete"
    This reverts commit 2777ced93bcad0b227c0591d0d2b9ef3c7953640.

--- app/assets/javascripts/bookings.js
+++ app/assets/javascripts/bookings.js
@@ -7,37 +7,4 @@ $(document).on('ready page:load', function(){
 // 		);
   // $(function() {
     $("#date_part").datepicker({dateFormat: "dd/mm/yy"});
-
-  
-			$("#subject_select").autocomplete({
-				delay: 500,
-				minLength: 1,
-				source: function(request, response) {
-					$.getJSON("/subjects/find", {
-						term: request.term
-					}, function(data) {
-						// data is an array of objects and must be transformed for autocomplete to use
-						var array = data.error ? [] : $.map(data, function(m) {
-							return {
-								value: m.name,
-								id: m.id
-							};
-						});
-						response(array);
-					});
-				},
-				focus: function(event, ui) {
-					// prevent autocomplete from updating the textbox
-					// event.preventDefault();
-				},
-				select: function(event, ui) {
-					$("#subject_id").val(ui.item.id)
-				}
-			});
-			// clear out the subject ID field if the search field is cleared
-			$("#subject_select").blur(function() {
-				if(!$(this).val()) {
-					$("#subject_id").val("");
-				}
-			});
 });
