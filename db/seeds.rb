8db141b62b1681990e63371931d8190db5da669f
Jack Kelly <s3449789@student.rmit.edu.au>
2016-04-26 23:59:14 +1000

    Adding search link to nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,14 @@ Presenter.delete_all
 Customer.delete_all
 Booking.delete_all
 Subject.delete_all
+Availability.delete_all
 
 Rake::Task['import:school_info'].invoke
 
 # Need this for the phone numbers and stuff.
 Faker::Config.locale = 'en-AU'
 
-40.times do |f|
+20.times do |f|
   word = (Faker::Hipster.words(2)* ' ').titleize
   while Subject.where(name: word).present?
     word = (Faker::Hipster.words(2)* ' ').titleize
@@ -91,25 +92,45 @@ c.save(:validate => false)
 
   presenter.school_info = SchoolInfo.all.sample
 
-
+  
   presenter.presenter_profile = PresenterProfile.create(bio: Faker::Hacker.say_something_smart,
                                                         status: "approved")
   a.presenter = presenter
-  3.times do  
+  rand(1..3).times do  
     subject = Subject.all.sample
     a.presenter.subjects << subject if !a.presenter.subjects.include? subject
   end
-
+  
 
   a.save(:validate => false)
 end
+
+50.times do
+    s = rand(1440)
+    e = rand(s..1440)
+    Availability.create(
+
+              start_time: s,
+              end_time: e,
+              monday: Faker::Boolean.boolean,
+              tuesday: Faker::Boolean.boolean,
+              wednesday: Faker::Boolean.boolean,
+              thursday: Faker::Boolean.boolean,
+              friday: Faker::Boolean.boolean,
+              saturday: Faker::Boolean.boolean,
+              sunday: Faker::Boolean.boolean,
+              presenter: Presenter.all.sample
+      )
+end
+
 5.times do |f|
   a = User.new(
              user_type: :customer,
              status: :pending,
              email: "example#{f+10}@gmail.com",
              password:              "password",
-             password_confirmation: "password")
+             password_confirmation: "password",
+             confirmed_at: Time.now )
   customer = Customer.create(phone_number:Faker::PhoneNumber.phone_number, 
                                first_name: Faker::Name.first_name,
                                last_name: Faker::Name.last_name, 
