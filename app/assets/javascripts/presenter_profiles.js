aaafe49b597736ff4ffc982baaadd5c4fcc24edb
Jack Kelly <s3449789@student.rmit.edu.au>
2016-05-11 23:17:18 +1000

    submit button on profile edit now disabled until changes are made

---
+++ app/assets/javascripts/presenter_profiles.js
@@ -0,0 +1,30 @@
+$(document).on('ready page:load', function(){
+	// $("#presenter_profile_bio_edit").on('input', function(){
+	// 	// $("$submitBtn").prop('disabled', false);
+	// 	alert();
+	// 	$("$submitBtn").removeClass('disabled');
+	// });
+
+(function() {
+  if (typeof tinyMCE != 'undefined') {
+    tinyMCE.init({
+      selector: "textarea.tinymce",
+      toolbar: ["styleselect | bold italic underline | bullist numlist | undo redo"],
+      style_formats: [{"title":"Headers","items":[{"title":"Heading 1","format":"h3"},{"title":"Heading 2","format":"h4"}]},{"title":"Inline","items":[{"title":"Bold","icon":"bold","format":"bold"},{"title":"Italic","icon":"italic","format":"italic"},{"title":"Underline","icon":"underline","format":"underline"},{"title":"Superscript","icon":"superscript","format":"superscript"},{"title":"Subscript","icon":"subscript","format":"subscript"}]}],
+      menubar: false,
+      statusbar: false,
+      setup : function(ed) {
+                  ed.on('change', function(e) {
+                   $("#submitBtn").prop('disabled', false);
+                   $("#submitBtn").removeClass('disabled');
+                   $("#submitBtn2").prop('disabled', false);
+                   $("#submitBtn2").removeClass('disabled');
+                  });
+            }
+    });
+  } else {
+    setTimeout(arguments.callee, 50);
+  }
+})();
+
+});
