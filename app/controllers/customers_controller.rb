93ace9ac3a0c437a790d42760baefb5741dfc127
Jack Kelly <s3449789@student.rmit.edu.au>
2016-05-17 18:59:51 +1000

    adding auth to customer dash

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,6 @@
 class CustomersController < ApplicationController
-	before_filter :has_access, :only => [:show]
+	before_filter :has_access?, :only => [:show]
+  before_filter :is_customer?, :only => [:index]
 
   def index
   	@search_params = params();
@@ -13,7 +14,15 @@ class CustomersController < ApplicationController
 	end
 
   private
-    def has_access
+    def has_access?
       redirect_to root_url unless (current_user.user_type == 'admin' || current_user.customer == Customer.find(params[:id]))
+    end
+
+    def is_customer?
+      if !current_user
+        redirect_to root_url
+      elsif !current_user.customer?
+        redirect_to root_url
+      end
     end
 end
