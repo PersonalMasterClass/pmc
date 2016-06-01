19ae96f23d00744a9e9eaa1ade5b0469dcad6785
Tyler Goodwin <tyler.goodwin23@gmail.com>
2016-06-01 15:29:44 +1000

    update admin nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     if current_user 
       if current_user.admin?
-        # redirect_to :controller => 'users', :action => 'index'
+        redirect_to :controller => 'users', :action => 'index'
       elsif current_user.presenter?
         redirect_to :controller => 'presenters', :action => 'index'
       elsif current_user.customer?
