d7cc01d30f62461869dfc77ea8baed9b94456240
Tyler Goodwin <tyler.goodwin23@gmail.com>
2016-06-01 15:22:32 +1000

    stop suspended users logging in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  def edit_presenter_profile_link(presenter, class_name = '')
-    link_to "Edit Profile",  new_presenter_profile_path(presenter), 'data-no-turbolink' => true, class: class_name
+  def edit_presenter_profile_link(presenter, class_name = '', text = 'Edit Profile' )
+    link_to text,  new_presenter_profile_path(presenter), 'data-no-turbolink' => true, class: class_name
   end
 end
