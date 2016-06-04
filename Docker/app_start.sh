c41e3c71015cf7052fcdb74fad26ceff3d6dd776
Jordan Kueh <jordan@zerocube.com.au>
2016-06-04 16:40:00 +1000

    Facilitating client-requested frontend changes

--- Docker/app_start.sh
+++ Docker/app_start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 source "$(git rev-parse --show-toplevel)/Docker/app_source.sh"
-for component in "${APP_COMPONENTS[@]}"; do
-	NAME="${APP_PREFIX}_${component}"
-	docker run -d --name "${NAME}" "${NAME}"
-done && 
+docker run -d -p 5432:5432 --name "${APP_PREFIX}_postgres" "${APP_PREFIX}_postgres" &&
+docker run -d -p 6379:6379 --name "${APP_PREFIX}_redis" "${APP_PREFIX}_redis" &&
 sleep 2 &&
-docker run -d -p 3000:3000 -e RAILS_ENV=development -e REDIS_URL="redis://${APP_PREFIX}_redis" --link="${APP_PREFIX}_redis" --link="${APP_PREFIX}_postgres" --name "${APP_PREFIX}" "${APP_PREFIX}";
+docker run -d -p 3000:3000 -e RAILS_ENV=development -e REDIS_URL="redis://localhost" --link="${APP_PREFIX}_redis" --link="${APP_PREFIX}_postgres" --name "${APP_PREFIX}" "${APP_PREFIX}";
