22aa77a060f51b6e092ab8cf7664e8dbc3dcf754
Jordan Kueh <s3432215@student.rmit.edu.au>
2016-06-05 16:47:44 +1000

    Working through client's frontend changes, and attempting to resolve the seeding error (unable to connect to redis on 127.0.0.1:6379) when using Docker

--- Docker/app_start.sh
+++ Docker/app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source "$(git rev-parse --show-toplevel)/Docker/app_source.sh"
-docker run -d -p 5432:5432 --name "${APP_PREFIX}_postgres" "${APP_PREFIX}_postgres" &&
-docker run -d -p 6379:6379 --name "${APP_PREFIX}_redis" "${APP_PREFIX}_redis" &&
+docker run -d --name "${APP_PREFIX}_postgres" "${APP_PREFIX}_postgres" &&
+docker run -d --name "${APP_PREFIX}_redis" "${APP_PREFIX}_redis" &&
 sleep 2 &&
-docker run -d -p 3000:3000 -e RAILS_ENV=development -e REDIS_URL="redis://localhost" --link="${APP_PREFIX}_redis" --link="${APP_PREFIX}_postgres" --name "${APP_PREFIX}" "${APP_PREFIX}";
+docker run -d -p 3000:3000 -e RAILS_ENV=development -e REDIS_URL="redis://${APP_PREFIX}_redis" --link="${APP_PREFIX}_redis" --link="${APP_PREFIX}_postgres" --name "${APP_PREFIX}" "${APP_PREFIX}";
