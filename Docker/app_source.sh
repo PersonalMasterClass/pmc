f0229206787122b2444226df4392f5e47b9f6822
Jordan Kueh <s3432215@student.rmit.edu.au>
2016-06-16 16:23:34 +1000

    Small update to the previous one - Prevent access denied errors on some systems where docker-machine is present, but not used (e.g. The current testing server uses Debian)

--- Docker/app_source.sh
+++ Docker/app_source.sh
@@ -2,6 +2,7 @@
 export APP_PREFIX="pmc"
 export WORKING_DIR="$(pwd)"
 export REPO_ROOT="$(git rev-parse --show-toplevel)"
-if [ -f $(which docker-machine 2>/dev/null) ]; then
-	eval $(docker-machine env)
+DM="$(which docker-machine)"
+if [ "${DM}" != "" ] && [ -f "${DM}" ] && [ -x "${DM}" ]; then
+	eval $(${DM} env)
 fi
