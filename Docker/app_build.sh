6bf8c206b95eadc714febbaf3506ace817fd4f42
Jordan Kueh <s3432215@student.rmit.edu.au>
2016-06-16 16:18:39 +1000

    Updating the Docker scripts so that it will default to using a Dockerfile_test (not tracked in the repository) if present, to allow us to test with APIs that have sensitive credentials. Also updated the landing page image provided by the client.

--- Docker/app_build.sh
+++ Docker/app_build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 source "$(git rev-parse --show-toplevel)/Docker/app_source.sh"
+DFILE="${REPO_ROOT}/Dockerfile"
+TESTFILE="${DFILE}_test"
+
+if [ -f "${TESTFILE}" ]; then
+	echo "Building with test file '${TESTFILE}'"
+	DFILE="${TESTFILE}"
+fi
+
 cd "${REPO_ROOT}" &&
-docker build -t "${APP_PREFIX}" "${REPO_ROOT}" &&
+docker build -t "${APP_PREFIX}" -f "${DFILE}" "${REPO_ROOT}" &&
 docker build -t "${APP_PREFIX}_postgres" -f "${REPO_ROOT}/Docker/Dockerfile_postgres" "${REPO_ROOT}" &&
 docker build -t "${APP_PREFIX}_redis" -f "${REPO_ROOT}/Docker/Dockerfile_redis" "${REPO_ROOT}" &&
 cd "${WORKING_DIR}"
--- Docker/app_source.sh
+++ Docker/app_source.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ -f $(which docker-machine 2>/dev/null) ]; then
-	eval $(docker-machine env)
-fi
 export APP_PREFIX="pmc"
 export WORKING_DIR="$(pwd)"
 export REPO_ROOT="$(git rev-parse --show-toplevel)"
+if [ -f $(which docker-machine 2>/dev/null) ]; then
+	eval $(docker-machine env)
+fi
