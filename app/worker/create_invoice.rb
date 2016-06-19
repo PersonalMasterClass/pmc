809736461431d7ac398deaabc3fd9c9cddc0480e
Andrew Tran <s3455476@student.rmit.edu.au>
2016-06-19 19:41:42 +1000

    Only send invoice if booking is confirmed

--- app/worker/create_invoice.rb
+++ app/worker/create_invoice.rb
@@ -3,9 +3,11 @@ class CreateInvoice
   # Background job for invoicing a booking
   def self.perform(booking_id)
     booking = Booking.find(booking_id)
-    unless booking.invoice!
-    	Notification.notify_admin("Something went wrong with with the invoicing for booking ##{booking_id}", booking_path(@booking), :invoice_fail)
-    end
+    if booking.chosen_presenter.present?
+	    unless booking.invoice!
+	    	Notification.notify_admin("Something went wrong with with the invoicing for booking ##{booking_id}", booking_path(@booking), :invoice_fail)
+	    end
+	  end
 
 	end
 end
